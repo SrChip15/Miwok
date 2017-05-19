51426e55563719c576229fec6f93770c89eaadd2
k3rn3l16 <k3rn3l16@outlook.com>
2017-05-19 03:27:58 -0700

    Hook up Color activity to an list adapter view and remove unnecessary layouts

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,32 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.word_list);
+
+        // Add ArrayList labelled colors containing strings
+        ArrayList<Word> colors = new ArrayList<Word>();
+        colors.add(new Word("red", "weṭeṭṭi"));
+        colors.add(new Word("green", "chokokki"));
+        colors.add(new Word("brown", "ṭakaakki"));
+        colors.add(new Word("gray", "ṭopoppi"));
+        colors.add(new Word("black", "kululli"));
+        colors.add(new Word("white", "kelelli"));
+        colors.add(new Word("dusty yellow", "ṭopiisә"));
+        colors.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        // Use Adapter to display
+        WordAdapter wordAdapter = new WordAdapter(this, colors);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(wordAdapter);
+
     }
 }
