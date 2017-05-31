904be8de8c1eb3251ea2d5697d0370b2a6cca5ea
k3rn3l16 <k3rn3l16@outlook.com>
2017-05-31 03:18:10 -0700

    Add tabbed layout

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,13 +1,18 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-	public CategoryAdapter(FragmentManager fm) {
+	private Context mContext;
+
+
+	public CategoryAdapter(Context context, FragmentManager fm) {
 		super(fm);
+		mContext = context;
 	}
 
 	/**
@@ -34,5 +39,19 @@ public class CategoryAdapter extends FragmentPagerAdapter {
 	@Override
 	public int getCount() {
 		return 4;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		// Generate title based on item position
+		if (position == 0) {
+			return mContext.getString(R.string.category_numbers);
+		} else if (position == 1) {
+			return mContext.getString(R.string.category_family);
+		} else if (position == 2) {
+			return mContext.getString(R.string.category_colors);
+		} else {
+			return mContext.getString(R.string.category_phrases);
+		}
 	}
 }
